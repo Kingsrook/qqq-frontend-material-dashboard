2a8bed1093376bac0e4003b802b6192449e3d7a2
Darin Kelkhoff <darin.kelkhoff@gmail.com>
2023-11-16 10:12:45 -0600

    CE-740 Undo padding-left for non-card labels... i want it for labels on parents, but it made wrong inside-tabs...

--- src/qqq/components/widgets/Widget.tsx
+++ src/qqq/components/widgets/Widget.tsx
@@ -462,7 +462,7 @@ function Widget(props: React.PropsWithChildren<Props>): JSX.Element
    //////////////////////////////////////////////////////////////////////////////////////////
    const labelToUse = props.widgetData?.label ?? props.widgetMetaData?.label
    let labelElement = (
-      <Typography sx={{cursor: "default", pl: props.widgetMetaData.isCard ? "auto" : "1rem", pt: props.widgetMetaData.type == "parentWidget" ? "1rem" : "auto"}} variant="h6" display="inline">
+      <Typography sx={{cursor: "default", pl: "auto", pt: props.widgetMetaData.type == "parentWidget" ? "1rem" : "auto"}} variant="h6" display="inline">
          {labelToUse}
       </Typography>
    );
